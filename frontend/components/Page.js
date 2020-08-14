5283e4ce78ae2d472c083b3c49f26d89713b074e
David <dsi28@hotmail.com>
2020-08-14 13:52:21 -0500

    add style-components to /components/Page.js as a test

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Meta from './Meta';
+import styled from 'styled-components';
+
+const MyButton = styled.button`
+    background: red;
+    font-size: ${props => (props.huge ? '100px' : '50px')};
+    span{
+        color:yellow
+    }
+`;
 
 class Page extends Component {
     render() {
@@ -8,7 +17,7 @@ class Page extends Component {
             <div>
                 <Meta/>
                 <Header/>
-                <p>Page  Component</p>
+                <MyButton huge><span>Click Me</span></MyButton>
                 {this.props.children}
             </div>
         )
